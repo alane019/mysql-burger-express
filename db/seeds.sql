6fcb432d90a2e2c13159d3c3e39532908e785040
Adam L <alane019@gmail.com>
2021-01-24 17:56:35 -0500

    added seed.sql with initial table data

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+insert into burgers(burger_name, devoured)
+values ("Awesome Burger", false);
+
+insert into burgers(burger_name, devoured)
+values ("Good Burger", false);
+
+insert into burgers(burger_name, devoured)
+values ("OK Burger", false);
+
+insert into burgers(burger_name, devoured)
+values ("Not So Good Burger", false);
