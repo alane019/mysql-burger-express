08d85b33bd6e6f3e784d80951b04b69664003a4d
Adam L <alane019@gmail.com>
2021-01-24 19:38:21 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3006,
+    port: 3306,
     user: 'root',
     password: 'root',
     database: 'burgers_db'
