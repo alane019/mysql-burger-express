72230b5aa87ef19d941f862971aed8ee894ad258
Adam L <alane019@gmail.com>
2021-01-24 19:32:15 -0500

    add config/connection.js and export connection module

---
+++ config/connection.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3006,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+});
+
+    connection.Connect((err) => {
+        if (err) throw err;
+
+            console.log(` user: ${connection.config.user}`);
+            console.log(` connected to database: ${connection.config.database}`);
+            console.log(`   port: ${connection.config.port}`);
+            console.log(`   host: ${connection.config.host}`);  
+    }); 
+
+    module.exports = connection;  
+
+
+
+
+
+
+
+
+
+
+
+
+
