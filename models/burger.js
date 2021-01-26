9c25c5a9aa63ea36429cea846813c18dd1790aca
Adam L <alane019@gmail.com>
2021-01-25 20:02:44 -0500

    add models directory

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+ const orm = require('../config/orm.js');
+
+ var burger = {
+
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            console.log(res);
+            cb(res);
+        });
+    },
+
+    insertOne: function(col, val, cb){
+        orm.insertOne("burgers", col, val, function(res){
+            console.log(res);
+            cb(res);
+        });
+    },
+
+     //ORM params:  targetTable, col1, val1, cb;
+    updateOne: function(col, val, cb){
+        orm.insertOne("burgers", col, val, function(res){
+            console.log(res);
+            cb(res);
+        });
+    }
+
+ };
+ module.exports = orm; 
