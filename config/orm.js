01c5b783380e8b42612a37281f7a2524ad8dfd15
Adam L <alane019@gmail.com>
2021-01-25 19:48:40 -0500

    update orm.js callback parameters

--- config/orm.js
+++ config/orm.js
@@ -3,30 +3,34 @@ const connection = require('./connection');
 // object relational mapper (orm)
 
 var orm = {
-
-    selectAll: function(targetTable){
+    selectAll: function(targetTable, cb){
         var queryString = "SELECT * from ??";   console.log(queryString);
         connection.query(queryString, [targetTable], function(err, result){
             if (err) throw err; 
             console.log(result);
+            cb(result);
         });
     },
 
-    insertOne: function(targetTable, col1, val1){
-        var queryString = "INSERT INTO ?? (??) VALUES (?)";   console.log(queryString);
+    insertOne: function(targetTable, col1, val1, cb){
+        var queryString = "INSERT INTO ?? (??) VALUES (?)"; console.log(queryString);
         connection.query(queryString, [targetTable, col1, val1], function(err, result){
             if (err) throw err; 
             console.log(result);
+            cb(result);
         });
     },
 
-    updateOne: function(targetTable, col1, val1){
+    updateOne: function(targetTable, col1, val1, cb){
         var queryString = "UPDATE ??  SET ??  = ?";   console.log(queryString);
         connection.query(queryString, [targetTable, col1, val1], function(err, result){
             if (err) throw err; 
+            cb(result);
             console.log(result);
         });
     }
+
+
 
 } 
 
